54da77158d35581aeb5496db924f60dcab9ec0f0
Alejandro Robayo <1249@holbertonschool.com>
2019-12-10 13:45:46 -0500

    [Fix] method to get last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastDig = int(repr(number)[-1]) * (-1 if number < 0 else 1)
+lastDig = (number if number >= 0 else -number) % 10 * (-1 if number < 0 else 1)
 msg = "and is " + (
     "greater than 5" if lastDig > 5
     else "0" if lastDig == 0 else "less than 6 and not 0")
