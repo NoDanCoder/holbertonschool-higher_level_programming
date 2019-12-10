f37078a0cc1a840fc234f03f934f4c6934f93d66
Alejandro Robayo <1249@holbertonschool.com>
2019-12-10 17:39:18 -0500

    [Add] print uppercase string

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,3 @@ def uppercase(str):
     str = [chr(ord(c) - 32) if ord(c) in range(ord('a'), ord('z') + 1) else c
            for c in str]
     print("".join(str))
-
-uppercase("56 cE MaMaroN k BroNeS 99   ")
