5d48be07123d7884fe4cf79321d4a344f43b234a
Alejandro Robayo <1249@holbertonschool.com>
2019-12-10 12:20:34 -0500

    [Fix] format to print

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,4 +5,4 @@ lastDig = int(repr(number)[-1])
 msg = "and is " + (
     "greater than 5" if lastDig > 5
     else "0" if lastDig == 0 else "less than 6 and not 0")
-print("Last digit of", repr(number), "is", repr(lastDig), msg)
+print("Last digit of {0:d} is {1:d} {2}".format(number, lastDig, msg))
