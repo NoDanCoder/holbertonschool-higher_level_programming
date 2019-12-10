6160a43affd00fcef5c7324cc7091582c65b847b
Alejandro Robayo <1249@holbertonschool.com>
2019-12-10 17:47:07 -0500

    [Fix] String format print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(str):
     str = list(str)
     str = [chr(ord(c) - 32) if ord(c) in range(ord('a'), ord('z') + 1) else c
            for c in str]
-    print("".join(str))
+    print("{}".format("".join(str)))
