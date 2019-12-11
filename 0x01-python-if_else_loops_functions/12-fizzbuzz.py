8d6c90ce78cdef90b8ecd50ee2095f0066f43221
Alejandro Robayo <1249@holbertonschool.com>
2019-12-10 19:25:14 -0500

    [Feature] simplified

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for x in range(1, 101):
-        msg = "Fizz" if x % 3 == 0 else ""
-        msg += "Buzz" if x % 5 == 0 else ""
+        msg = ("Fizz" if x % 3 == 0 else "") + ("Buzz" if x % 5 == 0 else "")
         msg = str(x) if not msg else msg
         print("{} ".format(msg), end='')
