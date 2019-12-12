055dfb8e7bf1b9455195fa8abbf0a5a8e7a1c571
Alejandro Robayo <1249@holbertonschool.com>
2019-12-12 12:13:46 -0500

    [Feature] four: short the code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import hidden_4
-    objs = sorted(dir(hidden_4))
-    for item in objs:
+    for item in sorted(dir(hidden_4)):
         if not item.startswith("__"):
             print("{}".format(item))
