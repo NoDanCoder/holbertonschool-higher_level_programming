eab047012195b2867d6b8248ffdeadd2ebd2ec9b
Alejandro Robayo <1249@holbertonschool.com>
2019-12-17 22:28:23 -0500

    [Fix] str format to print_matrix_integer func

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
     function that prints a matrix of integers
     """
     for y in matrix:
-        print(" ".join(["{}".format(x) for x in y]))
+        print(str.format(" ".join(["{}".format(x) for x in y])))
