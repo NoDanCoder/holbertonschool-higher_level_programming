9dd58ce72ac16b5455fd65991a107ebdb7df9af8
Alejandro Robayo <1249@holbertonschool.com>
2019-12-18 19:25:49 +0000

    [Fix] one : optimized

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,5 +3,5 @@ def element_at(my_list, idx):
     """
     function that retrieves an element from a list like in C
     """
-    if idx >= 0 and idx < len(my_list):
+    if idx in range(len(my_list)):
         return my_list[idx]
