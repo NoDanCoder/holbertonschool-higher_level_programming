0f8f83d0362772e47b6148af9d4b9d6dc0a5e53a
Alejandro Robayo <1249@holbertonschool.com>
2019-12-18 19:22:44 +0000

    [Fix] zero : optimized

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,4 @@ def print_list_integer(my_list=[]):
     """
     function that prints all integers of a list
     """
-    for x in my_list:
-        print("{:d}".format(x))
+    [print("{:d}".format(x)) for x in my_list]
