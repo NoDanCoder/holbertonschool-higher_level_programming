249e443b274ab648b375d2c98692fbd7f301cd98
Alejandro Robayo <1249@holbertonschool.com>
2019-12-20 14:35:35 +0000

    [Fix] ten : get key to max func done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,4 @@ def best_score(a_dictionary):
     """
     function that returns a key with the biggest integer value
     """
-    return max(a_dictionary) if a_dictionary else None
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
