ff62bb60c13bd538b783c8475af085318e084a91
Alejandro Robayo <1249@holbertonschool.com>
2019-12-20 03:54:50 +0000

    [Fix] twelve : number input

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,9 +3,11 @@ def roman_to_int(roman_string):
     """
     function that converts a Roman numeral to an integer
     """
-    num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    word = list(reversed(roman_string)) if roman_string else ""
-    add = num[word[0]] if roman_string else 0
-    for i, n in zip(word, word[1:]):
-        add += num[n] * (1 if num[i] <= num[n] else -1)
-    return add
+    if roman_string and isinstance(roman_string, str):
+        num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        word = list(reversed(roman_string))
+        add = num[word[0]]
+        for i, n in zip(word, word[1:]):
+            add += num[n] * (1 if num[i] <= num[n] else -1)
+        return add
+    return 0
