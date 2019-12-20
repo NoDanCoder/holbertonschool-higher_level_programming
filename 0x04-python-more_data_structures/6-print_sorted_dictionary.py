061ec21e22c5c8700b7687eb736b2c2cb229e21b
Alejandro Robayo <1249@holbertonschool.com>
2019-12-20 00:22:25 +0000

    [Add] six : print_sorted_dictionary func done

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """
+    function that prints a dictionary by ordered keys
+    first parent level only
+    """
+    [print(k, ":", v) for k, v in sorted(a_dictionary.items())]
