80bf452dea41bc15318cb21b6d77dff444b12672
Alejandro Robayo <1249@holbertonschool.com>
2019-12-20 00:37:01 +0000

    [Add] twelve : roman_to_int func done

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """
+    function that converts a Roman numeral to an integer
+    """
+    num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    word = list(reversed(roman_string))
+    add = num[word[0]]
+    for i, n in zip(word, word[1:]):
+        add += num[n] * (1 if num[i] <= num[n] else -1)
+    return add
