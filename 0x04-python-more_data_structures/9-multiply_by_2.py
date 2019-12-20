290c76701368a2633005d5f1b80ef2aaed0929b1
Alejandro Robayo <1249@holbertonschool.com>
2019-12-20 04:03:52 +0000

    [Fix] nine : empty input

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,4 +4,5 @@ def multiply_by_2(a_dictionary):
     function that returns a new dictionary with all values
     multiplied by 2
     """
-    return {k: (v * 2) for k, v in a_dictionary.items()}
+    if a_dictionary:
+        return {k: (v * 2) for k, v in a_dictionary.items()}
