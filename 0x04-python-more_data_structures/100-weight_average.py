9001a22b43316b7e6fedb692a57fe3d1b8c86f98
Alejandro Robayo <1249@holbertonschool.com>
2019-12-20 14:43:23 +0000

    [Fix] 100 : mul func added instead lambda

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+def mul(x, y):
+    """
+    function that returns "x" multiplied by "y"
+    """
+    return x * y
+
+
 def weight_average(my_list=[]):
     """
     function that returns the weighted average of all integers
     tuple (<score>, <weight>)
     """
-    mul = lambda x, y: x * y
     return (sum([mul(*x) for x in my_list]) / sum([x[1] for x in my_list])
             if my_list else 0)
