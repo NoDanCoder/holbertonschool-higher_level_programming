b66a634b60ac3eae0926ea5518b4121b19da1ea3
Alejandro Robayo <1249@holbertonschool.com>
2019-12-20 03:39:30 +0000

    [Fix] six : empty dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,5 @@ def print_sorted_dictionary(a_dictionary):
     function that prints a dictionary by ordered keys
     first parent level only
     """
-    [print(k, ":", v) for k, v in sorted(a_dictionary.items())]
+    if a_dictionary:
+        [print(k, ":", v) for k, v in sorted(a_dictionary.items())]
