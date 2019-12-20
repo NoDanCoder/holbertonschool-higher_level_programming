5a6067358b70cbeac713266a5dbb039aa8b2f79a
Alejandro Robayo <1249@holbertonschool.com>
2019-12-20 03:46:25 +0000

    [Fix] 102 : empty inputs

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,5 +3,7 @@ def complex_delete(a_dictionary, value):
     """
     function that deletes keys with a specific value in a dictionary
     """
-    [a_dictionary.pop(k) for k, v in a_dictionary.copy().items() if v == value]
+    if a_dictionary and value:
+        [a_dictionary.pop(k) for k, v in a_dictionary.copy().items()
+         if v == value]
     return a_dictionary
