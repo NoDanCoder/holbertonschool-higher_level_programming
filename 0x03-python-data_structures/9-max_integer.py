d7f74ec13a94367d3f8ccdc31812bc4460866bcb
Alejandro Robayo <1249@holbertonschool.com>
2020-01-07 23:47:02 -0500

    [Fix] start from 0 to print the max value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,4 @@ def max_integer(my_list=[]):
     """
     function that finds the biggest integer of a list
     """
-    add = 0
-    for i in my_list:
-        add = i if add < i else add
-    return add if my_list else None
+    return None if not my_list else sorted(set(my_list))[-1]
