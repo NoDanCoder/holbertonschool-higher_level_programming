b686590163211ff4256106eefb09d847558e3f44
Alejandro Robayo <1249@holbertonschool.com>
2020-01-08 15:44:03 -0500

    [Fix] pep8 style safe_print_integer func

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     """ prints safety a number """
-    try :
+    try:
         print("{:d}".format(value))
         return True
     except ValueError:
