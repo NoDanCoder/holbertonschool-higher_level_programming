a00f1d4be4a2806352276b307d5a56a3dc66dcc9
Alejandro Robayo <1249@holbertonschool.com>
2020-01-07 23:12:58 -0500

    [Fix] add_tuple func add max 2 values

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,4 +7,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     """
     list_a = list(tuple_a) + ([0] * (-len(tuple_a) + 2))
     list_b = list(tuple_b) + ([0] * (-len(tuple_b) + 2))
-    return tuple(x + y for (x, y) in zip(list_a, list_b))
+    return tuple(x + y for (x, y, z) in zip(list_a, list_b, range(2)))
