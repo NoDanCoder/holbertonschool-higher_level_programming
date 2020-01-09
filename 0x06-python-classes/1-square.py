528a6841118e77fee45a45c29cf5290c02c18331
Alejandro Robayo <1249@holbertonschool.com>
2020-01-09 17:11:02 -0500

    [Add] 1-square topic done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """ empty class Square that defines a square """
+
+    def __init__(self, size):
+        """ inizialize Square class """
+
+        self._Square__size = size
