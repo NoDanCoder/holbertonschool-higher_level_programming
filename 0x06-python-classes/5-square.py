7cb6b7c5140740552bb4eb043d0d52d93363f97e
Alejandro Robayo <1249@holbertonschool.com>
2020-01-09 18:52:39 -0500

    [Add] print_square module

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Square:
+    """ empty class Square that defines a square """
+
+    def __init__(self, size=0):
+        """ inizialize Square class """
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        """ return the current square area """
+
+        return self._Square__size ** 2
+
+    @property
+    def size(self):
+        """ retrieve the size of Square obj """
+
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        """ set the size of Square obj """
+
+        self.__init__(value)
+
+    def my_print(self):
+        """ draw in stdout the square with the character # """
+
+        print("\n".join(["".join(["#" for x in range(self.size)])
+                        for y in range(self.size)]))
