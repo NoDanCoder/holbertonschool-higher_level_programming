37f890d3f63bd7798a244a6791761d12cf094009
Alejandro Robayo <1249@holbertonschool.com>
2020-01-08 19:25:24 -0500

    [Add] 100 - func done

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    """ prints safety a number """
+    from sys import stderr
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as err:
+        stderr.write("Exception: {}\n".format(err))
+        return False
