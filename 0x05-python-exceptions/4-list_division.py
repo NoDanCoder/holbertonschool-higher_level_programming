5b8a563a4a453f048e95f970cf8baa45e3fd7c8d
Alejandro Robayo <1249@holbertonschool.com>
2020-01-08 19:06:39 -0500

    [Fix] list_division unisialized variable

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,6 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
     and return a new one with the results
     """
     out = []
+    val = 0
     for i in range(list_length):
         try:
             val = my_list_1[i] / my_list_2[i]
