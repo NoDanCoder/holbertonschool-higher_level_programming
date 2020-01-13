dd18e63da492c789b1ae02f02a546c5c5f3a3bb4
Alejandro Robayo <1249@holbertonschool.com>
2020-01-13 17:29:03 +0000

    [Add] four fucn done

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+The square print module
+=======================
+this module has a print_square fucn just for now
+"""
+
+
+def print_square(size):
+    """
+    Funtion that prints a square with "#" according to size.
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    
+    print("\n".join(["#" * size] * size))
