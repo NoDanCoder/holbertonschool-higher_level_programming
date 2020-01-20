4d58571ad83ae69c89bd1e85585f06314052e6d1
Alejandro Robayo <1249@holbertonschool.com>
2020-01-20 17:46:09 -0500

    [Fix] seven pep8 style fixed

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class BaseGeometry:
     """ class BaseGeometry """
-    
+
     def area(self):
         """ incomplepublic class method """
         raise Exception("area() is not implemented")
