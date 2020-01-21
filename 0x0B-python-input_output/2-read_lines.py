2dc7dfc6a4539af9dd78b1ba16417dc09afa8a76
Alejandro Robayo <1249@holbertonschool.com>
2020-01-21 18:46:07 -0500

    [Add] 02 - print without '\n'

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,4 +3,4 @@ def read_lines(filename="", nb_lines=0):
     """ reads n lines of a text file (UTF8) and prints it to stdout """
     with open(filename, encoding="utf-8") as f:
         nb_lines = nb_lines if nb_lines > 0 else None
-        print("".join(f.readlines()[:nb_lines]))
+        print("".join(f.readlines()[:nb_lines]), end="")
