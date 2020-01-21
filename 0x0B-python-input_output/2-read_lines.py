19c1b6efcc352cdd2ada5c7b98e3987fd1d892cc
Alejandro Robayo <1249@holbertonschool.com>
2020-01-21 18:41:18 -0500

    [Fix] 02 - read issues

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -2,4 +2,5 @@
 def read_lines(filename="", nb_lines=0):
     """ reads n lines of a text file (UTF8) and prints it to stdout """
     with open(filename, encoding="utf-8") as f:
-        print(f.read(-1 if nb_lines == 0 else nb_lines))
+        nb_lines = nb_lines if nb_lines > 0 else None
+        print("".join(f.readlines()[:nb_lines]))
