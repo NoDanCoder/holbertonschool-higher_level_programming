2323f36a143ccde533d36e50436fc2b5794aa6cb
Alejandro Robayo <1249@holbertonschool.com>
2020-01-23 14:19:16 -0500

    [Add] 00 - Base class pep8 style issue solved

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 class Base:
-    """  """
+    """ Base class struct """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """  """
-        if id != None:
+        """ Base class constructor """
+        if id is not None:
             self.id = id
         else:
             type(self).__nb_objects += 1
