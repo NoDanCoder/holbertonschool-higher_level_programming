740a8a745ab6a3a4197c8a5962fd5fc41bd8345c
Alejandro Robayo <1249@holbertonschool.com>
2020-04-15 13:11:16 -0500

    [Add] 0x11 - 02 task done

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' Python script that get 'X-Request-Id' value from response head '''
+
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('ascii')
+
+    req = urllib.request.Request(url, data)
+    
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+
+        print(body.decode('utf-8'))
