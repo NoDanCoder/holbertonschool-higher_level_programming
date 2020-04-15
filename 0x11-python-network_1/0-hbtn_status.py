0ec46a5a8e8599e76be24c75e0be08d124b2f385
Alejandro Robayo <1249@holbertonschool.com>
2020-04-15 12:37:40 -0500

    [Add] 0x11 - 00 task done

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' Python script that fetches a url '''
+
+if __name__ == '__main__':
+    import urllib.request
+
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+        display = {'type': type(body),
+                   'content': body,
+                   'utf8 content': body.decode('utf-8')}
+        
+        print('Body response:')
+        
+        for k, v in display.items():
+            print('\t- {}: {}'.format(k, v))
